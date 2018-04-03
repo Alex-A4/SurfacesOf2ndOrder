5542aea9474639cc50db41ce5661b1329ab757e4
Alex Adrianov <alex.a4.25@yandex.ru>
2018-04-03 19:05:55 +0300

    Change logic of quadratic equation

--- src/surf2ndOrd/CurvesOf2ndOrder.java
+++ src/surf2ndOrd/CurvesOf2ndOrder.java
@@ -19,18 +19,18 @@ import static org.lwjgl.util.glu.GLU.gluPerspective;
  * @author alexa4
  */
 public class CurvesOf2ndOrder {
-    float a, b, d, g, h, j;
+    //Coefficients of surface equation
+    float a, b, c, d, e, f, g, h, i, j;
     //Displacement of the scene on coordinate axis
-    private static float dx = 0f, dy = 0f;
+    private static float dx = 0f, dy = 0f, dz = -6f;
     //Coefficients of plane equation
-    private static float pA, pB, pC;
-    //Lists of points of surface and plane
+    private static float pA, pB, pC, pD;
+    //Lists of points of curve
     private static ArrayList <Point> curvePoints;
-    private static ArrayList <Point> planePoints;
     //Display size
     private static final int width = 680, height = 480;
     //Range of values on axis
-    private static float range = 15f;
+    private static float range = 10f;
     //Increment of a function
     private static float incrOfFunc = 0.001f;
     
@@ -39,17 +39,26 @@ public class CurvesOf2ndOrder {
      * Constructor with all coefficients of surface
      * @param a coefficient for x^2
      * @param b coefficient for y^2
-     * @param d coefficient for x*y 
+     * @param c coefficient for z^2
+     * @param d coefficient for x*y
+     * @param e coefficient for y*z
+     * @param f coefficient for x*z 
      * @param g coefficient for x
      * @param h coefficient for y
+     * @param i coefficient for z
      * @param j free coefficient
      */
-    CurvesOf2ndOrder(float a, float b, float d, float g, float h, float j){
+    CurvesOf2ndOrder(float a, float b, float c, float d, float e, float f,
+            float g, float h, float i, float j){
         this.a = a;
         this.b = b;
+        this.c = c;
         this.d = d;
+        this.e = e;
+        this.f = f;
         this.g = g;
         this.h = h;
+        this.i = i;
         this.j = j;
     }
 
@@ -65,7 +74,7 @@ public class CurvesOf2ndOrder {
         Display.create();
         
         curvePoints = new ArrayList<Point>();
-        planePoints = new ArrayList<Point>();
+        //planePoints = new ArrayList<Point>();
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
         //очистка экрана в какой-то цвет
@@ -97,17 +106,11 @@ public class CurvesOf2ndOrder {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             glLoadIdentity();
             
-            glTranslatef(dx, dy, -4f);
+            glTranslatef(dx, dy, dz);
             
             drawAxis();
             
             drawCurve();
-            
-            glColor3f(0f, 0f, 0f);
-            glBegin(GL_LINES);
-            for (Point p: planePoints)
-                p.callPoint();
-            glEnd();
             
             Display.sync(60);
             Display.update();
@@ -136,16 +139,6 @@ public class CurvesOf2ndOrder {
                 } catch(QuadrEqualException e){
                 }
             }
-        
-        //(maxX) -> (minX)
-        try{
-            float yP = solutEquat2D(maxX);
-            CurvesOf2ndOrder.planePoints.add(new CurvesOf2ndOrder.Point(maxX, yP));
-            yP = solutEquat2D(minX);
-            CurvesOf2ndOrder.planePoints.add(new CurvesOf2ndOrder.Point(minX, yP));
-        }catch (NullPointerException ex){
-            ex.printStackTrace();
-        }
     }
     
     
@@ -155,10 +148,11 @@ public class CurvesOf2ndOrder {
      * @param b coefficient for Y
      * @param d free coefficient
      */    
-    public void setPlaneCoef(float a, float b, float c){
+    public void setPlaneCoef(float a, float b, float c, float d){
         this.pA = a;
         this.pB = b; 
         this.pC = c;
+        this.pD = d;
     }
     
       
@@ -187,25 +181,26 @@ public class CurvesOf2ndOrder {
     
     /**
      * The solution of quadratic equation in 2D
-     * By^2 + y(2Dx + 2H) + (Ax^2 + 2Gx + J) = 0
+     * y^2*(b + c*pB^2/pC^2 - pB*e/pC) + y*(d*x + h + (2pA*pB*x*c + 
+     * + 2pD*pB*c)/pC^2 - (pA*x*e + pD*e + pB*x*f + pB*i)/pC) + (a*x^2 + g*x + j + 
+     * + (pA^2*x^2*c + 2pD*pA*c + pD^2*c)/pC^2 - (pA*x^2*f + f*x*pD
+     * + pA*x*i + pD*i)/pC) = 0
      * @param x is abscissa
      * @return solution of quadratic equation
      */
     private float[] solutOfQuadEquat2D(float x) throws QuadrEqualException{
         float[] res;
-        float A = this.b;
-        float B = this.d*x + this.h;
-        float C = this.a*x*x + this.g*x + this.j;
+        float A = b + c*pB*pB/(pC*pC) - pB*e/pC;
+        float B = d*x + h + (2*pA*pB*x*c + 2*pD*pB*c)/(pC*pC) - 
+                (pA*x*e + pD*e + pB*x*f + pB*i)/pC;
+        float C = a*x*x + g*x + j + (pA*pA*x*x*c + 2*pD*pA*x*c + pD*pD*c)/(pC*pC)
+                - (pA*x*x*f + f*x*pD + pA*x*i + pD*i)/pC;
         
         if (A == 0 && B == 0)
             throw new QuadrEqualException("A and B is zero");
-        if (A == 0){
+        if (A == 0){    
             res = new float[1];
             res[0] = -C/B;
-        } else if (B == 0){
-            res = new float[2];
-            res[0] = (float)Math.sqrt(C/A);
-            res[1] = -(float)Math.sqrt(C/A);
         } else{
             float D = B*B - 4*A*C;
             if (D < 0)
@@ -222,32 +217,23 @@ public class CurvesOf2ndOrder {
         return res;
     }
     
-    
-    /**
- * The solution of equation in 2D
- * Ax + By + C = 0
- * y = -(Ax + C)/B
- * @param x is abscissa
- * @return coefficient y
- */
-    private float solutEquat2D(float x) throws NullPointerException{
-        float y = -(pA*x + pC) / pB;
-        return y;
-    }
-    
      
     /**
      * Processing keyboard and mouse events
      */
     private void processInput(){
         if (Keyboard.isKeyDown(Keyboard.KEY_LEFT))
-            dx +=0.1;
+            dx += 0.1;
         if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT))
-            dx -=0.1;
+            dx -= 0.1;
         if (Keyboard.isKeyDown(Keyboard.KEY_UP))
-            dy -=0.1;
+            dy -= 0.1;
         if (Keyboard.isKeyDown(Keyboard.KEY_DOWN))
-            dy +=0.1;
+            dy += 0.1;
+        if (Keyboard.isKeyDown(Keyboard.KEY_O))
+            dz += 0.1;
+        if (Keyboard.isKeyDown(Keyboard.KEY_L))
+            dz -= 0.1;
     }
    
     
@@ -264,9 +250,6 @@ public class CurvesOf2ndOrder {
             glBegin(GL_POINTS);
             glVertex3f(x/10, y/10, 0f);
             glEnd();
-        }
-        private void callPoint(){
-            glVertex3f(x/10, y/10, 0f);
         }
         Point(float x, float y){
             this.x = x;
